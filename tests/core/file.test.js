9301c9155653720e6658f9147a273283bf2861f7
ExLeonem <maksim.sandybekov@live.de>
2020-05-20 22:01:50 +0200

    remove cached files, add tests for storage

---
+++ tests/core/file.test.js
@@ -0,0 +1,12 @@
+const FsUtil = require('../../core/file');
+
+
+
+test("Read files...", () => {
+
+});
+
+
+test("Write files...", () => {
+
+});
--- tests/core/storage.test.js
+++ tests/core/storage.test.js
@@ -1,21 +1,87 @@
-const Storage = reuqire('../../core/');
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const Storage = require('../../core/storage');
 
-// Setup
+
+const dataPath = path.join(process.cwd(), "data");
+let storage;
+
+
+// Setup 
 beforeEach(() => {
+    storage = new Storage();
 
 });
 
 
-// Teardown: Delete the created files
+// Teardown
 afterEach(() => {
 
+    // Clear directory of existing files
+    fs.readdirSync(dataPath, {"withFileTypes": true}).forEach((element) => {
+
+        let itemPath = path.join(dataPath, element.name);
+
+        // Clean-up json files from directory
+        if (element.isFile()) {
+            fs.unlinkSync(itemPath);
+        }
+    });
 });
 
 
-test("Adding new file", () => {
+test("Initial files are created", () => {
+
+    let dataDirContent = fs.readdirSync(dataPath);
+
+    expect(dataDirContent.length).toBeGreaterThan(0);
 
 });
 
-test("", () => {
 
+test("Adding new key to default", () => {
+
+    let value = "hello";
+    storage.set("queue.var", value);
+
+    // Manually read the file and parse content to object
+    let filePath = path.join(dataPath, "queue.json");
+    let content = JSON.parse(fs.readFileSync(filePath));
+
+    expect(content.var).toBe(value);
+});
+
+
+test("Update single key under json file", () => {
+
+    let newValue = 2;
+    storage.set("queue.count", newValue);
+
+    let filePath = path.join(dataPath, "queue.json");
+    let content = JSON.parse(fs.readFileSync(filePath));
+
+    expect(content.count).toBe(23);
+});
+
+
+test("Update complete file", () => {
+
+    let newValue = {
+        count: 12,
+        members: []
+    };
+    storage.set("queue", newValue);
+
+    let filePath = path.join(dataPath, "queue.json");
+    let content = JSON.parse(fs.readFileSync(filePath));
+
+    expect(content.count).toBe(12);
+});
+
+
+test("Retrieve file content", () => {
+
+    let defaultMemberCount = storage.get("queue.count");
+    expect(defaultMemberCount).toBe(0);
 });
