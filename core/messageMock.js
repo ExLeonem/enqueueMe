cb0e85f8dd6496b33558582fc6142e4338f328ce
ExLeonem <maksim.sandybekov@live.de>
2020-06-03 22:42:48 +0200

    adding functions to mock class

--- core/messageMock.js
+++ core/messageMock.js
@@ -6,6 +6,8 @@
  * 
  * @author Makim Sandybekov
  * @date 30.05.2020
+ * 
+ * @class
  */
 class MessageMock {
 
@@ -17,54 +19,107 @@ class MessageMock {
         this.user = {};
         this.message = {};
         this.channel = {};
+        this.guildChannels = [];
+        this.guild = {};
     }
 
     /**
      * Create the final message mock and returns it.
      * 
-     * @return {Object}
+     * @return {Object} Return the create message object mock.
      */
     create() {
 
+        // Create user information
         let userKey = "member";
         if (this.direct) {
             userKey = "author";
         }
-
         this.message[userKey] = this.user;
+    
+        // Create guild information
         this.message["guild"] = this.guild;
-        this.message["channel"] = this.channel;
+        if (!this.message.guild.channels) {
+            this.message.guild["channels"] = {};
+        }
+        this.__setEachMethod();
+        this.__setFindMethod();
 
+        // Create channel information
+        this.message["channel"] = this.channel;
+        
+        return this.message;
     }
 
     setUser(id, name, discriminator) {
 
         this.user["id"] = id;
-        this.user["user"] = {}
+        this.user["user"] = {};
         this.user.user["username"] = name;
         this.user.user["discriminator"] = discriminator;
+
         return this;
     }
 
 
     /**
+     * Creates the guild property with objects in cache to iterate over. 
+     * Should only be used on mock message sent from a guild channel.
+     * Both guildChanels and channelCategory needs to be filled to create channels
+     * in the guild cache. If different channel-name/category  combinations are needed
+     * use the addChannel function.
      * 
      * @param {*} guildId 
-     * @param {*} guildName 
-     * @param {}
+     * @param {string[]} channelNames An array of channel names.
+     * @param {string} channelCategory The category under which the guild channels to put
+     * @return {Object} Reference to the current message object.
      */
-    setGuild(guildId, guildName) {
+    setGuild(guildId, channelNames = null, channelCategory = null) {
+        this.guild["id"] = guildId;
 
+        if (channelNames && channelCategory) {
+
+            let channels = this.__createGuildChannels(channelNames, channelCategory);
+            this.guildChannels = channels.concat(this.guildChannels);
+        }
+    
+        return this;
+    }
+
+
+    // __setGuildChannelCache() {
+
+    // }
+
+
+    /**
+     * Adding a channel to the guild property cache. 
+     * 
+     * @param {string} channelName Name of the channel
+     * @param {string} parentName Name of the parrent
+     */
+    addChannel(channelName, parentName) {
+        
+        this.guildChannels.push({
+            "name": channelName,
+            "parent": {
+                "name": parentName
+            },
+            "send": function(content) {
+                return content;
+            }
+        });
     }
 
 
     /**
-     * Create the channel configuration
+     * Set the current channel configuration.
      * 
      * @param {string} name 
-     * @param {string} categoryName 
+     * @param {string} categoryName
+     * @return {Object} Reference to the current message object. 
      */
-    setChannel(name, categoryName) {
+    setChannel(name, categoryName = "Default") {
 
         this.channel["name"] = name;
         this.channel["parent"] = {};
@@ -72,16 +127,103 @@ class MessageMock {
         this.channel["send"] = function(content) {
             return content;
         }
+
+        return this;
     }
 
 
     /**
      * Set the type of message to direct message.
      * 
+     * @return {Object} Returns the current message object.
      */
-    setDirect() {
-        this.direct = true;
+    setDirect(isDirect = true) {
+        this.direct = isDirect;
+        return this;
+        
     }
+
+
+
+    // ------------------------
+    // Utilities
+    // ------------------------
+
+    /**
+     * Creates the cache if it is none-existent.
+     * 
+     * @private
+     */
+    __createNonExistentCache() {
+        if (!this.guild.channels["cache"]) {
+            this.guild.channels["cache"] = {};
+        }
+    }
+
+    /**
+     * Create guild channels to iterate over.
+     * 
+     * @private
+     * @param {string} channelNames Array of channel names 
+     */
+    __createGuildChannels(channelNames, channelCategory) {
+
+        let elements = [];
+
+        for (name of channelNames) {
+            elements.push({
+                "name": name,
+                "parent": {
+                    "name": channelCategory
+                },
+                "send": function(content) {
+                    return content;
+                }
+            });
+        }
+
+        return elements;
+    }
+
+    /**
+     * Sets the find method for the guild channel cache
+     * 
+     * @private
+     */
+    __setFindMethod() {
+
+        this.__createNonExistentCache();
+        let elements = this.guildChannels;
+        this.guild.channels.cache["find"] = callback => {
+            
+            for (element of elements) {
+                
+                if (callback(element)) {
+                    return element;
+                }
+            }
+        }
+    }
+
+    /**
+     * Sets the each method for the guild channel cache
+     * 
+     * @private
+     */
+    __setEachMethod() {
+
+        this.__createNonExistentCache();
+        let elements = this.guildChannels;
+        this.guild.channels.cache["each"] = callback => {
+
+            for (element of elements) {
+                newElements = callback(element);
+            }
+
+            return elements;
+        }
+    }
+
 }
 
 
