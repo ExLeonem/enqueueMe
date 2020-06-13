2b5dacb25ae0cccf32b307e398a6348a1f958e03
ExLeonem <maksim.sandybekov@live.de>
2020-06-13 18:30:03 +0200

    - add mockin capabilities of guild members - add command tests  - dequeue  - enqueue  - listen

--- tests/command/listen.test.js
+++ tests/command/listen.test.js
@@ -5,12 +5,15 @@ const MessageMock = require('../../core/messageMock');
 const Storage = require('../../core/storage');
 const Formatter = require('../../core/formatter');
 const definitions = require('../../commands/definitions.json');
-
 const Listen = require('../../commands/listen');
-
 
 const listen = new Listen("listen");
 const storage = Storage.getInstance();
+
+
+beforeEach(() => {
+    storage.set("admin", {});
+});
 
 
 test("Block direct message", () => {
@@ -32,3 +35,56 @@ test("Block message from unconfigured channel/category", () => {
     expect(actual).toBe(expected);
 });
 
+
+test("Listen to queue, which is currently empty", () => {
+
+    storage.set("queue", {});
+    let userId = 234234;
+    let subscribeMessage = new MessageMock(userId)
+        .setUser(userId)
+        .setGuild(234234)
+        .addChannel("admin", "bot")
+        .setChannel("admin", "bot")
+        .create();
+
+    
+    let response = listen.execute(subscribeMessage, []);
+    let expected = listen.getResponse("startListen", userId);
+    expect(response).toBe(expected);
+});
+
+
+test("Stop listening", () => {
+
+    storage.set("queue", {});
+    let userId = 234234;
+    let subscribeMessage = new MessageMock(userId)
+        .setUser(userId)
+        .setGuild(234234)
+        .addChannel("admin", "bot")
+        .setChannel("admin", "bot")
+        .create();
+    
+    listen.execute(subscribeMessage, []);
+    let response = listen.execute(subscribeMessage, ["stop"]);
+    let expected = listen.getResponse("stopListen", userId);
+    expect(response).toBe(expected);
+});
+
+
+test("Already listening", () => {
+
+    storage.set("queue", {});
+    let userId = 234234;
+    let subscribeMessage = new MessageMock(userId)
+        .setUser(userId)
+        .setGuild(234234)
+        .addChannel("admin", "bot")
+        .setChannel("admin", "bot")
+        .create();
+    
+    listen.execute(subscribeMessage, []);
+    let response = listen.execute(subscribeMessage, []);
+    let expected = listen.getResponse("alreadyListen", userId);
+    expect(response).toBe(expected);
+});
